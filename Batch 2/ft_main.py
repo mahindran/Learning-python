6a42f581845b955085cd574faa9ce0b0187cd372
mahindran <mahindran@users.noreply.github.com>
2019-02-23 15:32:51 +0530

    simple read and write to excel

--- Batch 2/ft_main.py
+++ Batch 2/ft_main.py
@@ -1,4 +1,4 @@
-import openpyxl
+from openpyxl import load_workbook
 
 class user:
 
@@ -11,15 +11,48 @@ class user:
 class insert:
 
     def newFeedback():
-        pass
+        print("**Please insert below information**")
+        CompanyID = input("Company ID = ")
+        InrateAnalyst = input("Inrate analyst name = ")
+        SGAAnalyst  = input("SGA analyst name  = ")
+        FeedbackType = input("Feedback type = ")
+        Indicator = input("Indicator name = ")
+        InrateRemark = input("Inrate comment = ")
+        newRow =(CompanyID,InrateAnalyst,SGAAnalyst,FeedbackType,Indicator,InrateRemark,SGARemark)
+        wb = load_workbook(filename='ft_file.xlsx')
+        ws = wb['Sheet1']
+        ws.append(newRow)
+        print("Saved successfully!")
+        wb.save("ft_file.xlsx")
 
     def editFeedback():
         pass
 
+class response:
+
+    def newResponse():
+        check.all()
+        print("**Which feedback you want to respond to?**")
+        CompanyID = input("Mention company ID = ")
+
+    def editResponse():
+        pass
+
+
 class check:
 
     def all():
-        pass
+        wb = load_workbook(filename='ft_file.xlsx', read_only=True)
+        ws = wb['Sheet1']
+
+        for row in ws.rows:
+            row_text =""
+            for x in row:
+                row_text = row_text + str(x.value) + "||"
+            print(row_text)
+            # for cell in row:
+            #     print(cell.value)
+
 
     def byUser():
         pass
@@ -29,4 +62,8 @@ class check:
 
 
 if __name__ == "__main__":
-    print("Hello world!")
+
+
+    #insert.newFeedback()
+    response.newResponse()
+    #check.all()
