3a3c4248111993ad67b9eaf995c8d145572b1d33
mahindran <mahindran@users.noreply.github.com>
2019-02-23 13:58:37 +0530

    started working on small office project, this is main file

---
+++ Batch 2/ft_main.py
@@ -0,0 +1,34 @@
+# class main
+# class insert
+# class check
+
+class user:
+
+    def Inrate():
+        pass
+
+    def SGA():
+        pass
+
+class insert:
+
+    def newFeedback():
+        pass
+
+    def editFeedback():
+        pass
+
+class check:
+
+    def all():
+        pass
+
+    def byUser():
+        pass
+
+    def byStatus():
+        pass
+
+
+if __name__ == "__main__":
+    print("Hello world!")
